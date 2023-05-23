e43fb84e1f49baa2f4eb3eaf0b11d2044e33c3b4
이재정 <jj9535@naver.com>
2023-05-24 00:06:49 +0900

    데이터 위치 변경 실험

--- xlsx/getXlsxData.ts
+++ xlsx/getXlsxData.ts
@@ -1,12 +1,11 @@
 import readXlsxFile from "read-excel-file/node";
 
 export default async function getXlsxData() {
-  const getNineStationData = await readXlsxFile(
-    `./xlsx/전국 도시광역철도 역사 화장실 현황_20220630.xlsx`
-  );
-
-  const getStationData = [
-    ...getNineStationData.slice(1, getNineStationData.length),
-  ];
-  return getStationData;
+  // const getNineStationData = await readXlsxFile(
+  //   `./xlsx/전국 도시광역철도 역사 화장실 현황_20220630.xlsx`
+  // );
+  // const getStationData = [
+  //   ...getNineStationData.slice(1, getNineStationData.length),
+  // ];
+  // return getStationData;
 }
